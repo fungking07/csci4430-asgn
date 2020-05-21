acbccd8815f6a518c70b9ac469c0e42ba953e6df
Soapony <seanponyfung@gmail.com>
2020-05-21 19:44:04 +0800

    entry finish

--- asgn3/entry.c
+++ asgn3/entry.c
@@ -3,27 +3,113 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include "entry.h"
 #include <time.h>
 
-#define ONE_SEC ((clock_t)1000);
-
 struct Entry *head = NULL;
-struct Entry *current = NULL;
 
 //insert a new entry to entry table
 void insert(struct Entry new_one)
 {
     struct Entry *entry=(struct Entry*) malloc(sizeof(struct Entry));
-    entry->in_ip=new_one->in_ip;
-    entry->in_port=new_one->in_port;
-    entry->out_port=new_one->out_port;
-    entry->time=clock();
+    entry->src_ip=new_one.src_ip;
+    entry->src_port=new_one.src_port;
+    entry->tran_port=new_one.tran_port;
+    entry->tran_ip=new_one.tran_ip;
+    entry->time=time(NULL);
     entry->next=head;
     head=entry;
 }
 
 struct Entry *search(unsigned int port)
 {
-    
+    struct Entry *current = NULL;
+    if(head==NULL)
+        return NULL;
+    current=head;
+    while(current != NULL)
+    {
+        if(current->tran_port == port)
+            return current;
+        current=current->next;
+    }
+    return NULL;
+}
+
+void check_time()
+{
+    struct Entry *current = head;
+    struct Entry *prev;
+    time_t now=time(NULL);
+    double duration;
+    if(head!=NULL)
+    {
+        while(current !=NULL)
+        {
+            duration=difftime(now,current->time);
+            if(duration >= 10.0)
+            {
+                //last one time out
+                if(current->next == NULL)
+                {
+                    //also is the only one
+                    if(head == current)
+                    {
+                        free(current);
+                        head=NULL;
+                        current=NULL;
+                    }
+                    else
+                    {
+                        free(current);
+                        prev->next=NULL;
+                        current=NULL;
+                    }
+                }
+                //not the tail
+                else
+                {
+                    //cur is head
+                    if(head==current)
+                    {
+                        head=current->next;
+                        free(current);
+                        current=head;
+                    }
+                    //cur is in not head and tail
+                    else
+                    {
+                        prev->next=current->next;
+                        free(current);
+                        current=prev->next;
+                    }
+                }
+            }
+            else
+            {
+                prev=current;
+                current=prev->next;
+            }
+        }
+    }
+}
+
+void showTable()
+{
+    printf("NAT table:\n");
+    printf("  source IP - Port  |  translated IP - Port\n");
+
+    struct Entry *cur=head;
+    while(cur != NULL)
+    {
+        struct in_addr tmp;
+        tmp.s_addr = htonl(cur->src_ip);
+        printf(" (%s , %d) ",(char*)inet_ntoa(tmp),cur->src_port);
+        tmp.s_addr = htonl(cur->tran_ip);
+        printf(" (%s , %d) ",(char*)inet_ntoa(tmp),cur->tran_port);
+        printf("\n");
+        cur=cur->next;
+    }
+    printf("\n\n");
 }
--- asgn3/entry.h
+++ asgn3/entry.h
@@ -3,15 +3,17 @@
 #define __TABLE__
 
 struct Entry{
-    uint32_t in_ip;
-    uint16_t in_port;
-    uint16_t out_port;
-    int time;
+    uint32_t src_ip;
+    uint16_t src_port;
+    uint32_t tran_ip;
+    uint16_t tran_port;
+    time_t time;
     struct Entry *next;
 };
 
 void check_time();
 struct Entry *search(unsigned int port);
 void insert(struct Entry new_one);
+void showTable();
 
 #endif
